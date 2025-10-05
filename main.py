d54a289678718570c45c9ccc7e5f301f90f48ee5
Gujeah <bme-011-17@must.ac.mw>
2025-10-05 14:32:24 +0200

    added fastapi

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Form, Request
+from fastapi.responses import JSONResponse
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from enum import Enum
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from predict import predict_recommendation
+from database import db_init, save_feedback, get_all_feedback
+from fastapi.staticfiles import StaticFiles
+
+### we need to initialize our database
+db_init()
 
 app =FastAPI() 
+analyzer=SentimentIntensityAnalyzer()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates=Jinja2Templates(directory="templates")
 
-@app.get("/predict")
-async def predict():
-    return "hello"
+@app.get("/", response_class=HTMLResponse)
+async def show_form(request:Request):
+    return templates.TemplateResponse(
+        "review_form.html", 
+        {"request":request, "submitted":False}
+        )
+    
+
+@app.post("/submit_review")
+async def handle_review(request: Request,review_text:str = Form(...)):
+    result=predict_recommendation(review_text)
+
+    ### saving our review to our database
+    save_feedback( 
+        review_text=review_text,
+        is_likely=result["is_likely"],
+        confidence=result["confidence"]
+        )
+    return templates.TemplateResponse("review_form.html", {
+        "request": request,
+        "submitted": True
+    })
+    
--- predict.py
+++ predict.py
@@ -2,9 +2,37 @@ import joblib
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import numpy as np
 import scipy.sparse as sp 
+from pathlib import Path
 
-model=joblib.load("C:\Users\Ethel\Desktop\Shecodes\British-Airways\models\final_model.pkl")
-tfid=joblib.load("C:\Users\Ethel\Desktop\Shecodes\British-Airways\models\tfidf_vectorizer.pkl")
+MODEL_PATH=Path("models/final_model.pkl")
+VECTORIZER_PATH=Path("models/tfidf_vectorizer.pkl")
+model=joblib.load(MODEL_PATH)
+tfidf=joblib.load(VECTORIZER_PATH)
 
 analyzer = SentimentIntensityAnalyzer()
 
+def predict_recommendation(review_text:str): 
+    ### performing vectorization on the reviews
+    tfidf_features = tfidf.transform([review_text])
+    scores = analyzer.polarity_scores(review_text)
+    vader_features = np.array([[
+        scores['neg'],
+        scores['neu'],
+        scores['pos'],
+        scores['compound']
+    ]])
+
+    ####combining those polarity scores to the review 
+    combined=sp.hstack([tfidf_features, vader_features])
+    prediction = model.predict(combined)[0]
+    probabilities = model.predict_proba(combined)[0]
+    confidence = float(max(probabilities))
+
+    is_likely = bool(prediction == 1)
+    label = "Likely to Recommend" if is_likely else "Not Likely to Recommend"
+
+    return {
+        "is_likely": is_likely,
+        "label": label,
+        "confidence": confidence
+    }
